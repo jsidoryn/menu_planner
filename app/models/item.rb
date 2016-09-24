87f031b0e89bbbf8e4cd2aa38fd04e1cff968dfa
Jason Sidoryn <jason@katalyst.com.au>
2016-09-24 14:58:29 +0930

    ingredient relations working

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+	enum unit: { tbs: 0, tsp: 1, cup: 2 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,12 @@
 class Recipe < ApplicationRecord
-  belongs_to :shortlist, optional: true
+	belongs_to :shortlist, optional: true
+	has_many :items
+	has_many :ingredients, :through => :items
+
 	dragonfly_accessor :image
 
 	validates :title, presence: true
 
 	enum meal: { breakfast: 0, lunch: 1, dinner: 2, dessert: 3, snack: 4 }
+
 end
