ef7ca82388a222a03089f5d87de4ea0df0ea81f2
Jason Sidoryn <jason@katalyst.com.au>
2016-10-11 17:43:38 +1030

    removed js files except for cocoon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,1 @@
-// HIGH PRIORITY JAVASCRIPT INCLUDED AT TOP OF PAGE
-
-//= require ornament/core
-//= require utilities/show
-//= require components/tabs
-//= require components/fotorama
-//= require components/navigation-mobile
-//= require components/menu-aim
-//= require components/issuu
-//= require components/oslide
-//= require components/embed
-//= require components/menu-with-more
-//= require components/enhanced-form
 //= require cocoon
