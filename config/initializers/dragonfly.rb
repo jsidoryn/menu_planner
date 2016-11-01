c7287053b0890b754b37a0d698c0a921463ed3ac
Jason Sidoryn <jason@katalyst.com.au>
2016-11-01 16:12:09 +1030

    s3 config for dragonfly

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -8,9 +8,17 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  if Rails.env.development? || Rails.env.test?
+    datastore :file,
+              root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+              server_root: Rails.root.join('public')
+  else
+    datastore :s3,
+              bucket_name: "dragonfly-jason",
+              access_key_id: ENV['S3_KEY'],
+              secret_access_key: ENV['S3_SECRET'],
+              url_scheme: 'https'
+  end
 end
 
 # Logger
