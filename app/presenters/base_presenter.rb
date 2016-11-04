9810aa5b5e10e4842fe12075ae1a5d2086086d23
Jason Sidoryn <jason@katalyst.com.au>
2016-11-05 09:42:42 +1030

    presenter v2

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,10 +1,20 @@
-class BasePresenter < SimpleDelegator
-  def initialize(model, view)
-    @view = view
-    super(model)
+class BasePresenter
+  def initialize(object, template)
+    @object = object
+    @template = template
   end
 
-  def h
-    @view
+  def self.presents(name)
+    define_method(name) do
+      @object
+    end
+  end
+
+  # def h
+  #   @template
+  # end
+
+  def method_missing(*args, &block)
+    @template.send(*args, &block)
   end
 end
